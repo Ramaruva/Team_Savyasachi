846d94bff89dd7cae0fa138ca9890d75acd7d36e
Ramaruva <ramrj1428@gmail.com>
2021-08-06 21:56:37 +0530

    k

--- savyabackend/src/model/user.model.js
+++ savyabackend/src/model/user.model.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String,require:true },
-    type: { type: String,required:true },
+    type: { type: String,required:true }
   },
   { timestamps: true }
 );
