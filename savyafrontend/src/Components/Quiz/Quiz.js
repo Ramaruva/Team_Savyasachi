5c7a3b45d35fe0ca74121aac20f47516e17664ed
Sreekanth <kanthuachu@gmail.com>
2021-08-06 18:52:04 +0530

    Quiz form

---
+++ savyafrontend/src/Components/Quiz/Quiz.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import axios from 'axios'
+
+function FetchQuestions(){
+
+    const [questions, setQuestions] = React.useState("")
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FetchQuestions
---
+++ savyafrontend/src/Components/Quiz/QuizQuestions/Quiz.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function QuizQuestions(){
+    return (
+        <div>
+            Quiz
+        </div>
+    )
+}
+
+export default QuizQuestions
