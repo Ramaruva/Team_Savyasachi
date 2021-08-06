4936da33c7d116e719bfad7958479b26ec960ec5
Ramaruva <ramrj1428@gmail.com>
2021-08-07 03:30:04 +0530

    added teacher register and login features

---
+++ savyabackend/src/controller/teacher.controller.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const Teacher = require("../model/teacher.model");
+const { body, validationResult } = require("express-validator");
+const newToken = (user) => {
+  return jwt.sign({ id: user.id }, process.env.JWT_TEACHER);
+};
+
+router.post(
+  "/register",
+  body("name").isLength({ min: 1 }).withMessage("Enter the name"),
+  body("email").isEmail().withMessage("Please enter valid email address"),
+  body("password")
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage("Password length should be 8 or more"),
+  body("role").isLength({ min: 1 }).withMessage("please enter your role"),
+  async (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(401).json({ message: result.array()[0].param });
+    }
+    try {
+      let user = await Teacher.create(req.body);
+      const token = newToken(user);
+      return res.status(200).send({ data: user, token });
+    } catch (error) {
+      return res.status(500).send({ message: "Failed" });
+    }
+  }
+);
+
+router.post(
+  "/login",
+  body("email").isEmail().withMessage("Please enter valid email address"),
+  body("password")
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage("Password length should be 8 or more"),
+  async (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(401).json({ message: result.array()[0].param });
+    }
+    let user;
+    try {
+      user = await Teacher.findOne({ email: req.body.email }).exec();
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: "Email or Password is Incorrect" });
+      }
+      const match = await user.checkpassword(req.body.password);
+      if (!match) {
+        return res
+          .status(401)
+          .json({ message: "Email or Password is Incorrect" });
+      }
+      const token = newToken(user);
+      res.status(201).json({ status: "Success", token, userData: user });
+    } catch (err) {
+        console.log(err)
+      return res.status(500).json({ error: "Failed" });
+    }
+  }
+);
+
+
+module.exports=router;
---
+++ savyabackend/src/model/teacher.model.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+const teacherSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true },
+    quiz: { type: Boolean },
+  },
+  { timestamps: true }
+);
+
+teacherSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+
+  bcrypt.hash(this.password, 8, (err, hash) => {
+    if (err) return next(err);
+    this.password = hash;
+    next();
+  });
+});
+
+teacherSchema.methods.checkpassword = function (password) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, this.password, (err, same) => {
+      if (err) return reject(err);
+      resolve(same);
+    });
+  });
+};
+
+const Teacher = model("teacher", teacherSchema);
+module.exports = Teacher;
