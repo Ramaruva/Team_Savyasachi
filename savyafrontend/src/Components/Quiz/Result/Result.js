c829f44ad66de90c514bb8f18a5499c3940fb034
Sreekanth <kanthuachu@gmail.com>
2021-08-07 09:04:49 +0530

    Quiz Done

--- savyafrontend/src/Components/Quiz/Result/Result.js
+++ savyafrontend/src/Components/Quiz/Result/Result.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import {Button} from '@material-ui/core'
+import { AuthContext } from '../../ContextProvider/AuthContextProvider'
+import "./Result.css"
 
 function Result(){
+    const {score,setScore} = useContext(AuthContext);
+
     return (
-        <div>
-            Result
+        <div className="result">
+            <span className="title">
+                Final Score:{score}</span>
+            <Button
+               variant="contained"
+               color="secondary"
+               size="large"
+               style={{alignSelf: "center",marginTop:20}} 
+                href="/quiz/pages"
+            >
+                GO To Quiz Page
+            </Button>
         </div>
     )
 }
